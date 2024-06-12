878d604fc5e6efb01b8778b23f819009f439a73a
nan721 <88175488+nan721@users.noreply.github.com>
2024-06-12 11:45:03 +0800

    Add files via upload

---
+++ HPPO.py
@@ -0,0 +1,257 @@
+# 强化学习实验组。包含DDQN类，表征强化学习的智能体。
+# 首先，依赖完全随机的原则，生成多轮选择步骤，每一轮选择步骤的用户位置互不相同。
+# 在每一轮中，设置9步转换动作，并将每个动作及其4个shuffle、动作执行前的状态及其4个shuffle、动作执行后的状态及其4个shuffle，获得的奖励存入记忆池中。
+# 记忆池中存储的为state, action, reward, state_，并再存储是否是终止状态的指示符done。
+#
+# Double DQN，使用了两个完全一样的网络结构，用于进行网络软更新。
+# Eval网络为实时更新的网络，target网络为缓慢更新的网络，target的参数为原来的target网络与eval网络的加权平均。
+
+# 待存储结束后，开始学习阶段。
+# 学习在DDQN中的learn函数中实现，通过降低两个Q值之间的均方误差，来起到学习的效果。
+# 其中，第一个Q值定义为Q_target，其等于本状态的记忆池中的reward，加上乘上gamma后的、由target网络预测的执行最佳动作（最佳动作为state_输入Q_eval后的最大Q值的对应动作）后的state_的Q值；
+# 而第二个Q值定义为将state直接输入到Q_eval网络后得到的记忆池中action对应位置的Q值。
+# 学习结束后，执行软更新和EPSILON的衰减。直至最后基本上完全依靠神经网络判断，不再使用随机策略。
+
+# 本程序为核心程序，依赖即将在后面提到的记忆池程序buffer.py。
+from torch.distributions.normal import Normal
+from torch.nn.utils import clip_grad_norm_
+import copy
+from torch.distributions import Categorical
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import numpy as np
+from buffer import ReplayBuffer
+import math
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+LOG_STD_MAX = 0.0
+LOG_STD_MIN = -5.0
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, net_width):
+        super(Actor, self).__init__()
+
+        self.l1 = nn.Linear(state_dim, 256)
+
+        self.pi_d = nn.Linear(256, action_dim)
+        self.mean = nn.Linear(256, 1)
+        self.logstd = nn.Linear(256, 1)
+    def forward(self, state):
+        n = torch.relu(self.l1(state))
+
+        return n
+    def pi(self, state, softmax_dim=0):
+        n = self.forward(state)
+        prob = F.softmax(self.pi_d(n), dim=softmax_dim)
+        mean = torch.sigmoid(self.mean(n))
+        log_std = self.logstd(n)
+        log_std = torch.tanh(log_std)
+        log_std = LOG_STD_MIN + 0.5 * (LOG_STD_MAX - LOG_STD_MIN) * (log_std + 1)
+        log_std = log_std.clamp(-4, 15)
+        return prob,mean, log_std
+
+
+    def save_checkpoint(self, checkpoint_file):
+        torch.save(self.state_dict(), checkpoint_file, _use_new_zipfile_serialization=False)####保存模型等相关参数
+
+    def load_checkpoint(self, checkpoint_file):
+        self.load_state_dict(torch.load(checkpoint_file))####将预训练的参数权重加载到新的模型之中
+
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, net_width):
+        super(Critic, self).__init__()
+
+        self.C1 = nn.Linear(state_dim, 256)
+
+        self.C2 = nn.Linear(256, 1)
+
+    def forward(self, state):
+        v = torch.relu(self.C1(state))
+
+        v = self.C2(v)
+        return v
+    def save_checkpoint(self, checkpoint_file):
+        torch.save(self.state_dict(), checkpoint_file, _use_new_zipfile_serialization=False)####保存模型等相关参数
+
+    def load_checkpoint(self, checkpoint_file):
+        self.load_state_dict(torch.load(checkpoint_file))####将预训练的参数权重加载到新的模型之中
+
+
+class PPO_discrete(object):
+    def __init__(
+            self,
+            state_dim,net_width,
+            action_dim,device,checkpoint_dir,
+            gamma=0.99,
+            lambd=0.95,
+
+            lr=1e-4,
+            clip_rate=0.2,
+            K_epochs=10,
+            batch_size=64,
+            l2_reg=1e-3,
+            entropy_coef=1e-3,
+            adv_normalization=False,
+            entropy_coef_decay=0.99,
+    ):
+        self.device = device
+        self.checkpoint_dir = checkpoint_dir
+        self.actor = Actor(state_dim, action_dim, net_width).to(device)
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=1e-5)
+
+        self.critic = Critic(state_dim, net_width).to(device)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=1e-4)
+
+        self.s_dim = state_dim
+        self.data = []
+
+        self.gamma = gamma
+        self.lambd = lambd
+        self.clip_rate = clip_rate
+        self.K_epochs = K_epochs
+        self.optim_batch_size = batch_size
+        self.l2_reg = l2_reg
+        self.entropy_coef = entropy_coef
+        self.adv_normalization = adv_normalization
+        self.entropy_coef_decay = entropy_coef_decay
+
+    def select_action(self, state):
+        '''Stochastic Policy'''
+        with torch.no_grad():
+            state = torch.from_numpy(state).float().to(self.device)
+            #
+            pi,mean, log_std = self.actor.pi(state, softmax_dim=1)
+            std = log_std.exp()
+            normal = Normal(mean, std)
+            x_t = normal.rsample()
+            action_c = x_t.clamp(0,1)
+            all_log_prob_c = normal.log_prob(x_t)
+            all_log_prob_c -= torch.log(1.0 - action_c.pow(2) + 1e-8)
+            log_prob_c = all_log_prob_c
+
+
+            m = Categorical(pi)
+            prob_d = m.probs
+            action_d = m.sample().item()
+            pi_a = pi[0][action_d].item()
+            log_prob_d = torch.log(prob_d + 1e-8)
+
+        return action_d, pi_a,action_c, log_prob_c
+
+    def evaluate(self, state):
+        '''Deterministic Policy'''
+        with torch.no_grad():
+            pi = self.actor.pi(state, softmax_dim=0)
+            a = torch.argmax(pi).item()
+        return a, 1.0
+
+    def train(self,transition_dict):
+
+        actions_combined = torch.tensor(transition_dict['actions'], dtype=torch.float).to(device)
+        a = torch.tensor(actions_combined[:, :1], dtype=torch.int64)
+        c_action = actions_combined[:, 1:]
+        s = torch.tensor(transition_dict['states'],
+                         dtype=torch.float).to(self.device)
+         # 动作不再是float类型
+        old_prob_a = torch.tensor(transition_dict['pi_a'], dtype=torch.float).view(-1, 1).to(
+            self.device)
+        old_prob_a_c = torch.tensor(transition_dict['log_prob_c'], dtype=torch.float).to(
+            self.device)
+        old_prob_a_c = old_prob_a_c.squeeze(dim=1)
+        r = torch.tensor(transition_dict['rewards'],
+                               dtype=torch.float).view(-1, 1).to(self.device)
+        s_prime = torch.tensor(transition_dict['next_states'],
+                                   dtype=torch.float).to(self.device)
+        dw_mask = torch.tensor(transition_dict['dones'],
+                             dtype=torch.float).view(-1, 1).to(self.device)
+        done_mask = dw_mask
+        self.entropy_coef *= self.entropy_coef_decay  # exploring decay
+
+        ''' Use TD+GAE+LongTrajectory to compute Advantage and TD target'''
+        with torch.no_grad():
+            vs = self.critic(s)
+            vs_ = self.critic(s_prime)
+
+            '''dw(dead and win) for TD_target and Adv'''
+            deltas = r + self.gamma * vs_ * (1 - dw_mask) - vs
+            deltas = deltas.cpu().flatten().numpy()
+            adv = [0]
+
+            '''done for GAE'''
+            for dlt, mask in zip(deltas[::-1], done_mask.cpu().flatten().numpy()[::-1]):
+                advantage = dlt + self.gamma * self.lambd * adv[-1] * (1 - mask)
+                adv.append(advantage)
+            adv.reverse()
+            adv = copy.deepcopy(adv[0:-1])
+            adv = torch.tensor(adv).unsqueeze(1).float().to(device)
+            td_target = adv + vs
+            if self.adv_normalization:
+                adv = (adv - adv.mean()) / ((adv.std() + 1e-4))  # useful in some envs
+
+        """PPO update"""
+        # Slice long trajectopy into short trajectory and perform mini-batch PPO update
+        optim_iter_num = int(math.ceil(s.shape[0] / self.optim_batch_size))
+
+        for _ in range(self.K_epochs):
+            # Shuffle the trajectory, Good for training
+            perm = np.arange(s.shape[0])
+            np.random.shuffle(perm)
+            perm = torch.LongTensor(perm).to(device)
+            s, a, td_target, adv, old_prob_a = \
+                s[perm].clone(), a[perm].clone(), td_target[perm].clone(), adv[perm].clone(), old_prob_a[perm].clone()
+
+            '''mini-batch PPO update'''
+            for i in range(optim_iter_num):
+                index = slice(i * self.optim_batch_size, min((i + 1) * self.optim_batch_size, s.shape[0]))
+
+                '''actor update'''
+                # 离散
+                prob,mean, log_std = self.actor.pi(s[index], softmax_dim=1)
+                entropy = Categorical(prob).entropy().view(-1,1)
+                prob_a = prob.gather(1, a[index])
+                ratio = torch.exp(torch.log(prob_a) - torch.log(old_prob_a[index]))  # a/b == exp(log(a)-log(b))
+                surr1 = ratio * adv[index]
+                surr2 = torch.clamp(ratio, 1 - self.clip_rate, 1 + self.clip_rate) * adv[index]
+                a_loss =( -torch.min(surr1, surr2) - self.entropy_coef * entropy).mean()
+
+                # 连续
+                std = log_std.exp()
+                continue_action_dist_now = Normal(mean, std)
+
+                continue_action_dist_entropy = continue_action_dist_now.entropy().sum(1,keepdim=True)
+                continue_action_logprob_now = continue_action_dist_now.log_prob(c_action[index])
+                continue_ratios = torch.exp(
+                    continue_action_logprob_now.sum(1, keepdims=True) - old_prob_a_c[index].sum(1,keepdims=True))
+                continue_surr1 = continue_ratios * adv[index]
+                continue_surr2 = torch.clamp(continue_ratios, 1 - self.clip_rate, 1 + self.clip_rate) * adv[index]
+                continue_action_loss = (-torch.min(continue_surr1,
+                                                   continue_surr2) - self.entropy_coef * continue_action_dist_entropy).mean()
+
+                ac_loss = a_loss + continue_action_loss
+
+
+
+                self.actor_optimizer.zero_grad()
+                ac_loss.backward()
+                torch.nn.utils.clip_grad_norm_(self.actor.parameters(), 40)
+                self.actor_optimizer.step()
+
+                '''critic update'''
+                c_loss = (self.critic(s[index]) - td_target[index]).pow(2).mean()
+                for name, param in self.critic.named_parameters():
+                    if 'weight' in name:
+                        c_loss += param.pow(2).sum() * self.l2_reg
+
+                self.critic_optimizer.zero_grad()
+                c_loss.backward()
+                self.critic_optimizer.step()
+        return a_loss.mean(), c_loss, entropy
+    def save_models(self, episode):
+        self.actor.save_checkpoint(self.checkpoint_dir + 'Q_eval/DDQN_q_eval_{}.pth'.format(episode))
+        print('Saving Q_eval network successfully!')
+
+    def load_models(self, episode):
+        self.actor.load_checkpoint(self.checkpoint_dir + 'Q_eval/DDQN_q_eval_{}.pth'.format(episode))
+        print('Loading Q_eval network successfully!')
---
+++ HSAC.py
@@ -0,0 +1,250 @@
+# 强化学习实验组。包含DDQN类，表征强化学习的智能体。
+# 首先，依赖完全随机的原则，生成多轮选择步骤，每一轮选择步骤的用户位置互不相同。
+# 在每一轮中，设置9步转换动作，并将每个动作及其4个shuffle、动作执行前的状态及其4个shuffle、动作执行后的状态及其4个shuffle，获得的奖励存入记忆池中。
+# 记忆池中存储的为state, action, reward, state_，并再存储是否是终止状态的指示符done。
+#
+# Double DQN，使用了两个完全一样的网络结构，用于进行网络软更新。
+# Eval网络为实时更新的网络，target网络为缓慢更新的网络，target的参数为原来的target网络与eval网络的加权平均。
+
+# 待存储结束后，开始学习阶段。
+# 学习在DDQN中的learn函数中实现，通过降低两个Q值之间的均方误差，来起到学习的效果。
+# 其中，第一个Q值定义为Q_target，其等于本状态的记忆池中的reward，加上乘上gamma后的、由target网络预测的执行最佳动作（最佳动作为state_输入Q_eval后的最大Q值的对应动作）后的state_的Q值；
+# 而第二个Q值定义为将state直接输入到Q_eval网络后得到的记忆池中action对应位置的Q值。
+# 学习结束后，执行软更新和EPSILON的衰减。直至最后基本上完全依靠神经网络判断，不再使用随机策略。
+
+# 本程序为核心程序，依赖即将在后面提到的记忆池程序buffer.py。
+
+from torch.distributions.categorical import Categorical
+from torch.distributions.normal import Normal
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import numpy as np
+from buffer import ReplayBuffer
+import argparse
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# ALGO LOGIC: initialize agent here:
+LOG_STD_MAX = 0.0
+LOG_STD_MIN = -5.0
+parser = argparse.ArgumentParser(description='SAC with 2 Q functions, offline updates')
+parser.add_argument('--weights-init',
+                        default='kaiming',
+                        const='kaiming',
+                        nargs='?',
+                        choices=['xavier', "orthogonal", 'uniform', 'kaiming'],
+                        help='weight initialization scheme for the neural networks.')
+parser.add_argument('--bias-init',
+                        default='zeros',
+                        const='xavier',
+                        nargs='?',
+                        choices=['zeros', 'uniform'],
+                        help='weight initialization scheme for the neural networks.')
+
+args = parser.parse_args()
+def layer_init(layer, weight_gain=1, bias_const=0):
+    if isinstance(layer, nn.Linear):
+        if args.weights_init == "xavier":
+            torch.nn.init.xavier_uniform_(layer.weight, gain=weight_gain)
+        elif args.weights_init == "orthogonal":
+            torch.nn.init.orthogonal_(layer.weight, gain=weight_gain)
+        elif args.weights_init == "kaiming":
+            nn.init.kaiming_normal_(layer.weight, nonlinearity='relu')
+        if args.bias_init == "zeros":
+            torch.nn.init.constant_(layer.bias, bias_const)
+
+class Policy(nn.Module):
+    def __init__(self, input_shape, out_c, out_d):
+        super(Policy, self).__init__()
+        # self.embedding = nn.Embedding()
+        # self.embeddings = nn.Embedding(64, 60)
+        self.fc1 = nn.Linear(input_shape, 512)
+        self.mean = nn.Linear(512, out_c)
+        self.logstd = nn.Linear(512, out_c)
+
+        self.pi_d = nn.Linear(512, out_d)
+
+        self.apply(layer_init)
+
+    def forward(self, x, device):
+        x = torch.Tensor(x).to(device)
+        x = torch.relu(self.fc1(x))
+        mean = torch.sigmoid(self.mean(x))
+        log_std = self.logstd(x)
+        pi_d = self.pi_d(x)
+        log_std = torch.tanh(log_std)
+        log_std = LOG_STD_MIN + 0.5 * (LOG_STD_MAX - LOG_STD_MIN) * (log_std + 1)  # From SpinUp / Denis Yarats
+
+        return mean, log_std, pi_d
+    def get_action(self, x, device):
+        mean, log_std, pi_d = self.forward(x, device)
+        std = log_std.exp()
+
+        normal = Normal(mean, std)
+        x_t = normal.rsample()  # for reparameterization trick (mean + std * N(0,1))
+        action_c = x_t.clamp(0,1)
+        all_log_prob_c = normal.log_prob(x_t)
+        all_log_prob_c -= torch.log(1.0 - action_c.pow(2) + 1e-8)
+        log_prob_c = all_log_prob_c
+
+        dist = Categorical(logits=pi_d)
+        action_d = dist.sample()
+        prob_d = dist.probs
+        log_prob_d = torch.log(prob_d + 1e-8)
+
+        return action_c, action_d, log_prob_c, log_prob_d, prob_d
+
+    def save_checkpoint(self, checkpoint_file):
+        torch.save(self.state_dict(), checkpoint_file, _use_new_zipfile_serialization=False)####保存模型等相关参数
+
+    def load_checkpoint(self, checkpoint_file):
+        self.load_state_dict(torch.load(checkpoint_file))####将预训练的参数权重加载到新的模型之中
+
+
+class SoftQNetwork(nn.Module):
+    def __init__(self, input_shape, out_c, out_d, layer_init):
+        super(SoftQNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_shape + out_c, 512)
+        self.fc2 = nn.Linear(512, out_d)
+        self.apply(layer_init)
+
+    def forward(self, x, a, device):
+        x = torch.Tensor(x).to(device)
+        x = torch.cat([x, a], 1)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+class SAC:
+    ''' 处理离散动作的SAC算法 '''
+    def __init__(self, state_dim, out_c_dim, out_d_dim,  tau, gamma, device,checkpoint_dir):
+        # 策略网络
+        self.actor = Policy(state_dim, out_c_dim, out_d_dim).to(device)
+        # 第一个Q网络
+        self.critic_1 = SoftQNetwork(state_dim, out_c_dim, out_d_dim, layer_init).to(device)
+        # 第二个Q网络
+        self.critic_2 = SoftQNetwork(state_dim,  out_c_dim, out_d_dim, layer_init).to(device)
+        self.target_critic_1 = SoftQNetwork(state_dim, out_c_dim, out_d_dim, layer_init).to(device)  # 第一个目标Q网络
+        self.target_critic_2 = SoftQNetwork(state_dim, out_c_dim, out_d_dim, layer_init).to(device)  # 第二个目标Q网络
+        # 令目标Q网络的初始参数和Q网络一样
+        self.target_critic_1.load_state_dict(self.critic_1.state_dict())
+        self.target_critic_2.load_state_dict(self.critic_2.state_dict())
+
+        self.values_optimizer = optim.Adam(list(self.critic_1.parameters()) + list(self.critic_2.parameters()), lr=1e-4)
+        self.policy_optimizer = optim.Adam(list(self.actor.parameters()), lr=1e-3)
+
+        # 使用alpha的log值,可以使训练结果比较稳定
+        self.starget_entropy_c = -0.99
+        self.log_alpha_c = torch.zeros(1, requires_grad=True, device=device)
+        self.alpha_c = self.log_alpha_c.exp().detach().cpu().item()
+        self.a_optimizer = optim.Adam([self.log_alpha_c], lr=1e-4)
+
+        self.target_entropy_d = 0.1498
+        self.log_alpha_d = torch.zeros(1, requires_grad=True, device=device)
+        self.alpha_d = self.log_alpha_d.exp().detach().cpu().item()
+        self.a_d_optimizer = optim.Adam([self.log_alpha_d], lr=1e-4)
+
+        # self.target_entropy = target_entropy  # 目标熵的大小
+        self.gamma = gamma
+        self.tau = tau
+        self.device = device
+        self.out_d_dim = out_d_dim
+        self.out_c_dim = out_c_dim
+        self.user = 3
+        self.rrh = 10
+        self.checkpoint_dir = checkpoint_dir
+
+    def take_action(self, state):
+
+        state = torch.tensor(state, dtype=torch.float).to(self.device)
+        action_c, action_d, log_prob_c, log_prob_d, prob_d = self.actor.get_action(state,device)
+        action_d = action_d.item()
+        c = action_d
+        action_c_single = action_c[0].item()
+        return action_c_single,action_c, action_d
+
+    def norm(self, state):
+        state[0][state[0] == 0] = -1.0
+        state[1] = (state[1] - state[1].mean()) / state[1].std()
+        return state
+
+
+    def soft_update(self, net, target_net):
+        for param_target, param in zip(target_net.parameters(),
+                                       net.parameters()):
+            param_target.data.copy_(param_target.data * (1.0 - self.tau) +
+                                    param.data * self.tau)
+
+    def update(self, transition_dict):
+        states = torch.tensor(transition_dict['states'],
+                              dtype=torch.float).to(self.device)
+        actions_combined = torch.tensor(transition_dict['actions'], dtype=torch.float).to(device)
+        actions_d = actions_combined[:, :1]
+        actions_c = actions_combined[:, 1:]
+        rewards = torch.tensor(transition_dict['rewards'],
+                               dtype=torch.float).view(-1, 1).to(self.device)
+        next_states = torch.tensor(transition_dict['next_states'],
+                                   dtype=torch.float).to(self.device)
+        dones = torch.tensor(transition_dict['dones'],
+                             dtype=torch.float).view(-1, 1).to(self.device)
+        # 更新两个Q网络
+        with torch.no_grad():
+            next_state_actions_c, next_state_actions_d, next_state_log_pi_c, next_state_log_pi_d, next_state_prob_d = self.actor.get_action(next_states, device)
+            qf1_next_target = self.target_critic_1.forward(next_states, actions_c, device)
+            qf2_next_target = self.target_critic_2.forward(next_states, actions_c, device)
+
+            min_qf_next_target = next_state_prob_d * (torch.min(qf1_next_target, qf2_next_target) - self.alpha_c * next_state_prob_d * next_state_log_pi_c - self.alpha_d * next_state_log_pi_d)
+            next_q_value = torch.Tensor(rewards).to(
+                device) + (1 - torch.Tensor(dones).to(device)) * self.gamma * (min_qf_next_target.sum(1)).view(-1).reshape(32,1)
+        qf1_a_values = self.critic_1.forward(states, actions_c, device).gather(1, actions_d.long().view(-1, 1).to(
+            device)).squeeze().view(-1).reshape(32,1)
+        qf2_a_values = self.critic_2.forward(states, actions_c, device).gather(1, actions_d.long().view(-1, 1).to(
+            device)).squeeze().view(-1).reshape(32,1)
+        qf1_loss = F.mse_loss(qf1_a_values, next_q_value)
+        qf2_loss = F.mse_loss(qf2_a_values, next_q_value)
+        qf_loss = (qf1_loss + qf2_loss) / 2
+        self.values_optimizer.zero_grad()
+        qf_loss.backward()
+        self.values_optimizer.step()
+
+        # 更新策略网络
+        actions_c, actions_d, log_pi_c, log_pi_d, prob_d = self.actor.get_action(states, device)
+        qf1_pi = self.critic_1.forward(states, actions_c, device)
+        qf2_pi = self.critic_2.forward(states, actions_c, device)
+        min_qf_pi = torch.min(qf1_pi, qf2_pi)
+
+        policy_loss_d = (prob_d * (self.alpha_d * log_pi_d - min_qf_pi)).sum(1).mean()
+        policy_loss_c = (prob_d * (self.alpha_c * prob_d * log_pi_c - min_qf_pi)).sum(1).mean()
+        policy_loss = policy_loss_d + policy_loss_c
+
+        self.policy_optimizer.zero_grad()
+        policy_loss.backward()
+        self.policy_optimizer.step()
+
+        # 更新alpha值
+        with torch.no_grad():
+            a_c, a_d, lpi_c, lpi_d, p_d = self.actor.get_action(states, device)
+        alpha_loss = (-self.log_alpha_c * p_d * (p_d * lpi_c + self.starget_entropy_c)).sum(1).mean()
+        alpha_d_loss = (-self.log_alpha_d * p_d * (lpi_d + self.target_entropy_d)).sum(1).mean()
+
+        self.a_optimizer.zero_grad()
+        alpha_loss.backward()
+        self.a_optimizer.step()
+        self.alpha_c = self.log_alpha_c.exp().item()
+
+        self.a_d_optimizer.zero_grad()
+        alpha_d_loss.backward()
+        self.a_d_optimizer.step()
+        self.alpha_d = self.log_alpha_d.exp().item()
+
+
+        self.soft_update(self.critic_1, self.target_critic_1)
+        self.soft_update(self.critic_2, self.target_critic_2)
+
+    def save_models(self, episode):
+        self.actor.save_checkpoint(self.checkpoint_dir + 'Q_eval/DDQN_q_eval_{}.pth'.format(episode))
+        print('Saving Q_eval network successfully!')
+
+    def load_models(self, episode):
+        self.actor.load_checkpoint(self.checkpoint_dir + 'Q_eval/DDQN_q_eval_{}.pth'.format(episode))
+        print('Loading Q_eval network successfully!')
